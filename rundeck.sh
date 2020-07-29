ea35edae4292b1256df6979aa8655cca250124df
Jeeva Ram <55439939+imjeevaram@users.noreply.github.com>
2020-07-29 15:07:51 +0530

    Update rundeck.sh

--- rundeck.sh
+++ rundeck.sh
@@ -42,7 +42,7 @@ do
    		#  echo  -e "************ Job $CURRENT_JOB_ID is not running
  	   	 #Tiggered rundeck api with arguments using curl method		
 		  curl  -s -k -H "X-Rundeck-Auth-Token:$2"   -X POST   $URL_RUN  -d argString=-$5+$value%20-$6+$8
-		  sleep 5;
+		  #sleep 5;
                build_id[$count]=$(curl -s  -k -m $URL_TIME_OUT -H "X-Rundeck-Auth-Token:$2"    $URL_JOB_CHECK  | xmlstarlet sel -t -c "string(/result/executions/execution/@id)" | sed 's/%//')
 			   ((count+=1));	
 	   else
