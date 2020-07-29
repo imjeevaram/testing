f71ed4677fbfd0eb1ef60f5ddac93916d2561ec2
Jeeva Ram <55439939+imjeevaram@users.noreply.github.com>
2020-07-29 20:40:35 +0530

    Update rundeck.sh

--- rundeck.sh
+++ rundeck.sh
@@ -42,7 +42,7 @@ do
    		#  echo  -e "************ Job $CURRENT_JOB_ID is not running
  	   	 #Tiggered rundeck api with arguments using curl method		
 		  curl  -s -k -H "X-Rundeck-Auth-Token:$2"   -X POST   $URL_RUN  -d argString=-$5+$value%20-$6+$8
-               build_id[$count]=$(curl s -k -m $URL_TIME_OUT -H "X-Rundeck-Auth-Token:$2"    $URL_JOB_CHECK  | xmlstarlet sel -t -c "string(/executions/execution/@id)" | sed 's/%//')
+               build_id[$count]=$(curl -s -k -m $URL_TIME_OUT -H "X-Rundeck-Auth-Token:$2"    $URL_JOB_CHECK  | xmlstarlet sel -t -c "string(/executions/execution/@id)" | sed 's/%//')
 			   ((count+=1));	
 	   else
 		#display date & time for  waiting period
