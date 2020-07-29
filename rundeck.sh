90419e6cb8e77a74801ff807811a9fb3d0c4df4b
Jeeva Ram <55439939+imjeevaram@users.noreply.github.com>
2020-07-29 20:31:07 +0530

    Update rundeck.sh

--- rundeck.sh
+++ rundeck.sh
@@ -63,14 +63,15 @@ do
 							 ((count+=1));					  	
 				else
 					sleep $SLEEP_TIME;
-					echo -e "\n***Build status of rundeck job***\nProject_Name: $3 \nJob_Name: $4\n"  >> $OUTPUT_FILE
+					echo -e "\nHi,\n***Build status of rundeck job***\n\nProject_Name: $3 \nJob_Name: $4\n\n"  >> $OUTPUT_FILE
 					for id in "${build_id[@]}"
 					do
 						curl  -s  -k -m $URL_TIME_OUT -H "X-Rundeck-Auth-Token:$2"   $URL_JOB_STATUS/$id  | xmlstarlet sel -t   -o 'REGION:-' -c "string(/result/executions/execution/job/options/option[1]/@value)"  -o '   BUILD_ID:-'  -c "string(/result/executions/execution/@href)"   -o '    BUILD_STATUS:-' -c "string(/result/executions/execution/@status)"  | sed 's/%//'  >> $OUTPUT_FILE
+					        echo -e "\n" >> $OUTPUT_FILE
 					done
 					#Rundeck Job checking 
 					echo -e "\nRundeck PROJECT_NAME:$3 \n JOB_URL:$JOB_URL \n REGION:$value  \n Rundeck Job is running more than threshold.So please check rundeck server*******" >> $OUTPUT_FILE
-					echo -e "\nRegards,\nDevops Team" >> $OUTPUT_FILE
+					echo -e "\n\n\nRegards,\nDevops Team" >> $OUTPUT_FILE
 					cat  $OUTPUT_FILE
 					exit 
 			       fi		
@@ -78,11 +79,12 @@ do
 done
 echo -e "\n*********Job has been executed with all regions***********"
 sleep $SLEEP_TIME;
-echo -e "\n***Build status of rundeck job***\nProject_Name: $3 \nJob_Name: $4\n"  >> $OUTPUT_FILE
+echo -e "\nHi,\n***Build status of rundeck job***\n\nProject_Name: $3 \nJob_Name: $4\n\n"  >> $OUTPUT_FILE
 for id in "${build_id[@]}"
 do
 	curl   -s -k  -m $URL_TIME_OUT -H "X-Rundeck-Auth-Token:$2"    $URL_JOB_STATUS/$id  | xmlstarlet sel -t  -o "REGION:-" -c "string(/result/executions/execution/job/options/option[1]/@value)"  -o '   BUILD_ID:-'  -c "string(/result/executions/execution/@href)"   -o '    BUILD_STATUS:-' -c "string(/result/executions/execution/@status)"  | sed 's/%//'  >>  $OUTPUT_FILE
+        echo -e "\n" >> $OUTPUT_FILE
 done
-echo -e "\nRegards,\nDevops Team" >> $OUTPUT_FILE
+echo -e "\n\n\nRegards,\nDevops Team" >> $OUTPUT_FILE
 cat $OUTPUT_FILE
 fi
